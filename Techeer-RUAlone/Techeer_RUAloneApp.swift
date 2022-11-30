a5edaecf099ab56d56ad3e80d9f9943c6b80b8f9
홍성민 <seanhong2000@gmail.com>
2022-11-30 12:51:41 +0900

    Initial Commit

---
+++ Techeer-RUAlone/Techeer_RUAloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Techeer_RUAloneApp.swift
+//  Techeer-RUAlone
+//
+//  Created by Sean Hong on 2022/11/30.
+//
+
+import SwiftUI
+
+@main
+struct Techeer_RUAloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Techeer-RUAloneUITests/Techeer_RUAloneUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Techeer_RUAloneUITestsLaunchTests.swift
+//  Techeer-RUAloneUITests
+//
+//  Created by Sean Hong on 2022/11/30.
+//
+
+import XCTest
+
+final class Techeer_RUAloneUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
