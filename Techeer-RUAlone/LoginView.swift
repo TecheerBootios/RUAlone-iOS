a488ed1776d282b160fac8ca9ee6167c3a6486a0
HAERYN <syt06119@naver.com>
2022-12-12 21:04:24 +0900

    feat: #1 텍스트인풋, 로그인 버튼

--- Techeer-RUAlone/LoginView.swift
+++ Techeer-RUAlone/LoginView.swift
@@ -10,22 +10,51 @@ import SwiftUI
 struct LoginView: View {
     @State var emailAddress: String = ""
     @State var password: String = ""
-    
+
     var body: some View {
-      VStack {
-        Text("이메일 \(emailAddress)")
-        TextField("이메일", text: $emailAddress)
-          .padding()
-          .background(Color(uiColor: .secondarySystemBackground))
-          .textFieldStyle(.roundedBorder)
-        Text("패스워드 \(password)")
-        TextField("패스워드", text: $password)
-          .padding()
-          .background(Color(uiColor: .secondarySystemBackground))
-          .textFieldStyle(.roundedBorder)
-      }
+        VStack {
+            Text("로그인")
+                .font(.system(size: 30))
+            
+            TextField("이메일", text: $emailAddress)
+                .frame(maxWidth: 300)
+                .padding()
+                .background(Color.white)
+                .clipShape(RoundedRectangle(cornerRadius: 50))
+                .shadow(color: .gray, radius: 2, x: 0, y: 2)
+            
+            TextField("패스워드", text: $password)
+                .frame(maxWidth: 300)
+                .padding()
+                .foregroundColor(.black)
+                .background(Color.white)
+                .clipShape(RoundedRectangle(cornerRadius: 50))
+                .shadow(color: .gray, radius: 2, x: 0, y: 2)
+            
+            Button(action: {}) {
+                Text("로그인")
+                    .fontWeight(.bold)
+                    .frame(maxWidth: 300)
+                    .padding()
+                    .foregroundColor(.black)
+                    .background(Color.white)
+                    .clipShape(RoundedRectangle(cornerRadius: 50))
+                    .shadow(color: .gray, radius: 2, x: 0, y: 2)
+            }
+            
+            Button(action: {}) {
+                Text("카카오 로그인")
+                    .fontWeight(.bold)
+                    .frame(maxWidth: 300)
+                    .padding()
+                    .foregroundColor(.black)
+                    .background(Color.yellow)
+                    .clipShape(RoundedRectangle(cornerRadius: 50))
+                    .shadow(color: .gray, radius: 2, x: 0, y: 2)
+            }
+        }
     }
-  }
+}
 
 struct LoginView_Previews: PreviewProvider {
     static var previews: some View {
