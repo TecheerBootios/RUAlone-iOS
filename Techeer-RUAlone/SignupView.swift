50a284f4cbed78e7b57e28e7ca9c9e8887f4f859
HAERYN <syt06119@naver.com>
2022-12-20 03:39:03 +0900

    fix: #7 회원가입 화면 파일 수정

--- Techeer-RUAlone/SignupView.swift
+++ Techeer-RUAlone/SignupView.swift
@@ -8,8 +8,74 @@
 import SwiftUI
 
 struct SignupView: View {
+    @State var user: String = ""
+    @State var email: String = ""
+    @State var password: String = ""
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Color.customBackground
+            .ignoresSafeArea()
+            .overlay(
+                VStack {
+                    Image("MainIcon")
+                        .resizable()
+                        .frame(width: 200, height: 220)
+                    
+                    Text("회원가입")
+                        .font(.system(size:35, weight: .black))
+                    
+                    HStack {
+                        Image("User")
+                            .resizable()
+                            .frame(width: 30, height: 30)
+                            .padding(15)
+                        TextField("사용자 이름", text: $email)
+                    }
+                    .frame(width: 360, height: 60)
+                    .background(Color(uiColor: .white))
+                    .cornerRadius(50)
+                    
+                    HStack {
+                        Image("Email")
+                            .resizable()
+                            .frame(width: 30, height: 30)
+                            .padding()
+                        TextField("이메일", text: $email)
+                    }
+                    .frame(width: 360, height: 60)
+                    .background(Color(uiColor: .white))
+                    .cornerRadius(50)
+                    .padding(5)
+                    
+                    HStack {
+                        Image("Password")
+                            .resizable()
+                            .frame(width: 30, height: 30)
+                            .padding(20)
+                        TextField("패스워드", text: $password)
+                    }
+                    .frame(width: 360, height: 60)
+                    .background(Color(uiColor: .white))
+                    .cornerRadius(50)
+                    .padding(5)
+                    
+                    Button("회원 가입") { }
+                        .frame(width: 330, height: 25)
+                        .foregroundColor(Color.white)
+                        .padding()
+                        .background(Color.black)
+                        .cornerRadius(50)
+                        .padding(5)
+                        .shadow(color: .gray, radius: 5, x: 0, y: 5)
+                    
+                    Button("카카오 로그인") { }
+                        .frame(width: 330, height: 25)
+                        .foregroundColor(Color.black)
+                        .padding()
+                        .background(Color.customYellow)
+                        .cornerRadius(50)
+                        .shadow(color: .gray, radius: 5, x: 0, y: 5)
+                })
     }
 }
 
