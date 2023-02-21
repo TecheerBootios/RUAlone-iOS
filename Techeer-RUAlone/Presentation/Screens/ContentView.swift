dc080f54d427cfe522476f57607288e78222557b
HAERYN <syt06119@naver.com>
2023-02-21 22:27:25 +0900

    fix: #27 splash text 삭제

--- Techeer-RUAlone/Presentation/Screens/ContentView.swift
+++ Techeer-RUAlone/Presentation/Screens/ContentView.swift
@@ -30,10 +30,6 @@ struct ContentView: View {
                 }
                 .tint(.black)
             } else {
-//                Text("혼자 왔니 ?")
-//                    .font(.system(size: 40, weight: .light, design: .serif))
-//                    .frame(width: 300, height: 20)
-//                    .padding(EdgeInsets(top: 200, leading: 20, bottom: 0, trailing: 0))
                 LottieView(lottieFile: "women-thinking", contentMode: .scaleAspectFit)
                     .padding(EdgeInsets(top: 0, leading: 0, bottom: 100, trailing: 0))
             }
