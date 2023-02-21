aa1d446ba0bd86a34d0ac3fa90fcbbea59722984
HAERYN <syt06119@naver.com>
2023-02-21 20:37:35 +0900

    feat: #27 Splash Screen + Lottie 완전 적용 완료

--- Techeer-RUAlone/Presentation/Screens/ContentView.swift
+++ Techeer-RUAlone/Presentation/Screens/ContentView.swift
@@ -7,23 +7,44 @@
 
 import SwiftUI
 
-struct ContentView: View {    
+struct ContentView: View {
+    
+    @State var isActive: Bool = false
+    
     var body: some View {
-        TabView {
-            Home()
-                .tabItem {
-                    Label("Home", systemImage: "house.fill")
+        VStack{
+            if self.isActive {
+                TabView {
+                    Home()
+                        .tabItem {
+                            Label("Home", systemImage: "house.fill")
+                        }
+                    Feed()
+                        .tabItem {
+                            Label("Post", systemImage: "person.3.fill")
+                        }
+                    Message()
+                        .tabItem {
+                            Label("Message", systemImage: "message.fill")
+                        }
                 }
-            Feed()
-                .tabItem {
-                    Label("Post", systemImage: "person.3.fill")
-                }
-            Message()
-                .tabItem {
-                    Label("Message", systemImage: "message.fill")
-                }
+                .tint(.black)
+            } else {
+//                Text("혼자 왔니 ?")
+//                    .font(.system(size: 40, weight: .light, design: .serif))
+//                    .frame(width: 300, height: 20)
+//                    .padding(EdgeInsets(top: 200, leading: 20, bottom: 0, trailing: 0))
+                LottieView(lottieFile: "women-thinking", contentMode: .scaleAspectFit)
+                    .padding(EdgeInsets(top: 0, leading: 0, bottom: 100, trailing: 0))
+            }
         }
-        .tint(.black)
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2.5) {
+                withAnimation {
+                    self.isActive = true
+                }
+            }
+        }
     }
 }
 
